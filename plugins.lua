97d770e86e2aa0eb5c3b969a04094c819d92b828
don.nguyen <don.nguyen@astrodigital.com>
2023-12-27 15:49:19 -0800

    Trying mini surround

--- plugins.lua
+++ plugins.lua
@@ -116,6 +116,28 @@ local plugins = {
     "kdheepak/lazygit.nvim",
     cmd = "LazyGit",
   },
+
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    event = "VeryLazy",
+    config = function()
+      require("mini.surround").setup {
+        mappings = {
+          add = "sa", -- Add surrounding in Normal and Visual modes
+          delete = "sd", -- Delete surrounding
+          find = "sf", -- Find surrounding (to the right)
+          find_left = "sF", -- Find surrounding (to the left)
+          highlight = "sH", -- Highlight surrounding
+          replace = "sr", -- Replace surrounding
+          update_n_lines = "sn", -- Update `n_lines`
+
+          suffix_last = "l", -- Suffix to search with "prev" method
+          suffix_next = "n", -- Suffix to search with "next" method
+        },
+      }
+    end,
+  },
 }
 
 return plugins

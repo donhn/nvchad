82053514a620814dbaef342c3041622469ae2c71
ngdon <ngdon@pm.me>
2024-02-06 23:17:36 -0800

    relnum true, colorcolumn 81

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- Set relative number on by default.
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = "81"
+
+-- Identify shader files as glsl.
 vim.cmd [[
   au BufRead,BufNewFile *.vert,*.frag,*.geom set filetype=glsl
 ]]

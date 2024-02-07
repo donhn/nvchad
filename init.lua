22e09ebc8dfc847d70eb48f3e0e347950185246e
don.nguyen <don.nguyen@astrodigital.com>
2024-02-07 11:11:17 -0800

    Turn off colorcolumn for text files. Use powershell for shell commands when in Windows

--- init.lua
+++ init.lua
@@ -3,7 +3,17 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "81"
 
+-- powershell
+if vim.fn.has "win32" or vim.fn.has "win64" then
+  vim.o.shell = "powershell.exe"
+end
+
 -- Identify shader files as glsl.
 vim.cmd [[
   au BufRead,BufNewFile *.vert,*.frag,*.geom set filetype=glsl
+]]
+
+-- Turn off colorcolumn for text files.
+vim.cmd [[
+  au BufRead,BufNewFile *.txt set colorcolumn=0
 ]]

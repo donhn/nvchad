653502d078ad8944fd7aba508e3ea292472b0269
ngdon <ngdon@pm.me>
2024-02-07 11:21:39 -0800

    Use powershell when in windows

--- init.lua
+++ init.lua
@@ -5,7 +5,21 @@ vim.opt.colorcolumn = "81"
 
 -- powershell
 if vim.fn.has "win32" or vim.fn.has "win64" then
+  -- Set shell to powershell.exe
   vim.o.shell = "powershell.exe"
+
+  -- Set shellxquote
+  vim.o.shellxquote = ""
+
+  -- Set shellcmdflag to include necessary PowerShell flags
+  vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
+
+  -- Set shellquote to empty string as it's not needed for PowerShell
+  vim.o.shellquote = ""
+
+  -- Set shellpipe and shellredir to use Out-File with UTF8 encoding
+  vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
+  vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
 end
 
 -- Identify shader files as glsl.

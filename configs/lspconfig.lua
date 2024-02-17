74dc583383bdf471abfcb9116bce754e9cfa1dd3
Don <ngdon@pm.me>
2024-02-17 15:57:03 -0800

    remove glslls

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "bashls", "clangd", "jedi_language_server", "pyright", "cmake", "glslls" }
+local servers = { "bashls", "clangd", "jedi_language_server", "pyright", "cmake" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
